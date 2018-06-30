e24bc84d4eea6d0620efeff1ec83efda62fbf876
YYY <YYY@yyy.mshome.net>
2018-06-30 16:17:15 +0800

    打印 L401 修改客户端签名

--- src/com/newer/fileserver/App.java
+++ src/com/newer/fileserver/App.java
@@ -1,11 +1,13 @@
-package com.newer.fileserver;
+﻿package com.newer.fileserver;
 
 /**
  * 服务端
  * 
- * @author 
+ * @author JWwwwww
  *
  */
 public class App {
-
+	public static void main(String[] args) {
+		System.out.println("L401");
+	}
 }
