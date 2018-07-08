84da91915e3d1623cdda4dfb25c71ee1d18b2d6d
JWwwwww <40692522+JWwwwww@users.noreply.github.com>
2018-07-08 10:55:06 +0800

    实现客户端上传与服务器的接收储存

--- src/com/newer/fileserver/Client.java
+++ src/com/newer/fileserver/Client.java
@@ -1,5 +1,15 @@
 package com.newer.fileserver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
 /**
  * 客户端
  * 
@@ -8,4 +18,62 @@ package com.newer.fileserver;
  */
 public class Client {
 
+	// 文件输入流
+	FileInputStream fileInputStream;
+
+	// 套接字
+	Socket socket;
+
+	OutputStream out;
+
+	// 服务器地址及端口号
+	String serverAddress = "";
+	int serverPort = 9000;
+
+	public void start() {
+		// 输入需要上传的文件
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入文件：");
+		String file = sc.next();
+		sc.close();
+
+		try {
+			// 实例化套接字
+			socket = new Socket(serverAddress, serverPort);
+
+			// 通过套接字建立客户端向服务器的输出
+			out = socket.getOutputStream();
+
+			// 定义一个字节数组
+			byte[] buf = new byte[1024 * 4];
+			int size;
+			fileInputStream = new FileInputStream(file);
+			while (-1 != (size = fileInputStream.read(buf))) {
+				out.write(buf);
+				out.flush();
+			}
+			System.out.println("上传成功");
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fileInputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.start();
+	}
 }
--- src/com/newer/fileserver/Server.java
+++ src/com/newer/fileserver/Server.java
@@ -1,5 +1,19 @@
 package com.newer.fileserver;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 服务器
  * 
@@ -8,4 +22,72 @@ package com.newer.fileserver;
  */
 public class Server {
 
+	// 端口号
+	int port = 9000;
+
+	// 套接字
+	ServerSocket serverSocket;
+
+	// 线程池
+	ExecutorService pool;
+
+	String filePath = "E:\\新建文件夹";
+
+	public void start() {
+		try {
+			serverSocket = new ServerSocket(port);
+
+			// 建立一个可扩大的线程池
+			pool = Executors.newCachedThreadPool();
+
+			while (true) {
+				Socket socket = serverSocket.accept();
+				pool.execute(new Runnable() {
+
+					public void run() {
+
+						ByteArrayOutputStream data = new ByteArrayOutputStream();
+
+						// 通过输出流获得文件的数据
+						try (InputStream in = socket.getInputStream()) {
+							byte[] buf = new byte[1024 * 4];
+							int size;
+							while (-1 != (size = in.read(buf))) {
+								data.write(buf, 0, size);
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+
+						byte[] info = data.toByteArray();
+						String file = "";
+
+						// 计算文件的散列值
+						try {
+							byte[] hash = MessageDigest.getInstance("SHA-256").digest(info);
+							file = new BigInteger(1, hash).toString(16);
+						} catch (NoSuchAlgorithmException e) {
+							e.printStackTrace();
+						}
+
+						// 新建文件，文件名为散列值
+						try (FileOutputStream out = new FileOutputStream(new File(filePath, file))) {
+							out.write(info);
+						} catch (FileNotFoundException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Server server = new Server();
+		server.start();
+	}
 }
