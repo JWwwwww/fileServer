f15471afa9e9162d5827d6aa411b798261800e35
JWwwwww <40692522+JWwwwww@users.noreply.github.com>
2018-07-09 10:19:15 +0800

    修改服务器和客户端
    使服务器可以判断文件是否已经上传，如果已有则拒绝客户端的上传。

--- src/com/newer/fileserver/Client.java
+++ src/com/newer/fileserver/Client.java
@@ -1,13 +1,16 @@
 package com.newer.fileserver;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.math.BigInteger;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -44,16 +47,38 @@ public class Client {
 			// 通过套接字建立客户端向服务器的输出
 			out = socket.getOutputStream();
 
+			// 新建一个长度可变的字节数组
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
 			// 定义一个字节数组
 			byte[] buf = new byte[1024 * 4];
 			int size;
 			fileInputStream = new FileInputStream(file);
 			while (-1 != (size = fileInputStream.read(buf))) {
-				out.write(buf);
-				out.flush();
+				baos.write(buf, 0, size);
 			}
-			System.out.println("上传成功");
+			byte[] b = baos.toByteArray();
+			
+			// 计算文件的散列值
+			try {
+				byte[] hash = MessageDigest.getInstance("SHA-256").digest(b);
+				out.write(hash);
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
 
+			// 用一个长度为1的字节数组来接收信息
+			byte[] bt = new byte[1];
+			InputStream in = socket.getInputStream();
+			in.read(bt);
+			if (bt[0] == 1) {
+				System.err.println("不能重复上传!");
+			} else if (bt[0] == 0) {
+				out.write(b);
+				System.out.println("上传成功！");
+			}
+			out.close();
+			in.close();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
--- src/com/newer/fileserver/Server.java
+++ src/com/newer/fileserver/Server.java
@@ -6,11 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -33,6 +35,8 @@ public class Server {
 
 	String filePath = "E:\\新建文件夹";
 
+	HashMap<String, String> map = new HashMap<>();
+
 	public void start() {
 		try {
 			serverSocket = new ServerSocket(port);
@@ -50,31 +54,28 @@ public class Server {
 
 						// 通过输出流获得文件的数据
 						try (InputStream in = socket.getInputStream()) {
-							byte[] buf = new byte[1024 * 4];
-							int size;
-							while (-1 != (size = in.read(buf))) {
-								data.write(buf, 0, size);
+							byte[] buf = new byte[32];
+							in.read(buf);
+							String hash = new BigInteger(1, buf).toString(16);
+							OutputStream out = socket.getOutputStream();
+							byte[] bff = new byte[1];
+
+							// 如果hashmap里包含这个hash值，就发送1，否则发送0
+							if (map.containsKey(hash)) {
+								bff[0] = 1;
+								out.write(bff);
+							} else {
+								byte[] bff2 = new byte[1024 * 4];
+								bff[0] = 0;
+								out.write(bff);
+
+								// 将文件下载到本地
+								FileOutputStream out2 = new FileOutputStream(new File(filePath, hash));
+								while (-1 != in.read(bff2)) {
+									out2.write(bff2);
+								}
 							}
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-
-						byte[] info = data.toByteArray();
-						String file = "";
-
-						// 计算文件的散列值
-						try {
-							byte[] hash = MessageDigest.getInstance("SHA-256").digest(info);
-							file = new BigInteger(1, hash).toString(16);
-						} catch (NoSuchAlgorithmException e) {
-							e.printStackTrace();
-						}
-
-						// 新建文件，文件名为散列值
-						try (FileOutputStream out = new FileOutputStream(new File(filePath, file))) {
-							out.write(info);
-						} catch (FileNotFoundException e) {
-							e.printStackTrace();
+							map.put(hash, hash);
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
